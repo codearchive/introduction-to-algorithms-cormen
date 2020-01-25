87e309ec8d3f8c9a3a58b01ead35417a67d506c6
Pavel Patrusov <pavelp9415@gmail.com>
2020-01-25 23:58:21 +0200

    Added Stack implementation in C#

---
+++ chapter_3/section_10/subsection_10_1/c-sharp/StackImplementation/StackImplementation/MyStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    public class MyStack
+    {
+        private const int Size = 3;
+        private int[] _arr;
+        private int _capacity;
+        private int _top;
+
+        public MyStack(int size = Size)
+        {
+            _arr = new int[size];
+            _capacity = size;
+            _top = -1;
+        }
+
+        public void Push(int x)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Stack Overflow");
+            }
+            else
+            {
+                Console.WriteLine($"Insertion {x}");
+                _arr[++_top] = x;
+            }
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack Underflow");
+                return -1;
+            }
+            else
+            {
+                Console.WriteLine($"Removing {Peek()}");
+                return _arr[_top--];
+            }
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return -1;
+            }
+            else
+            {
+                return _arr[_top];
+            }
+        }
+
+        public int GetSize()
+        {
+            return _top + 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return _top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return _top == _capacity - 1;
+        }
+    }
+}
---
+++ chapter_3/section_10/subsection_10_1/c-sharp/StackImplementation/StackImplementation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyStack stack = new MyStack();
+
+            Console.WriteLine($"Is my stack empty?\n\t{stack.IsEmpty()}");
+            stack.Push(10);
+            Console.WriteLine($"Is my stack empty?\n\t{stack.IsEmpty()}");
+            stack.Push(20);
+            stack.Pop();
+            Console.WriteLine($"Stack size is: {stack.GetSize()}");
+            Console.WriteLine($"Is my stack full?\n\t{stack.IsFull()}");
+            stack.Push(30);
+            stack.Push(40);
+            Console.WriteLine($"Is my stack full?\n\t{stack.IsFull()}");
+            Console.ReadLine();
+        }
+    }
+}
